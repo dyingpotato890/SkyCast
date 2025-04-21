7d7a812783a69bdda1d06f2605d10390f03bc29b
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-04-22 00:58:11 +0530

    add logic for weather (not working)

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class WeatherApp extends StatefulWidget {
+  const WeatherApp({super.key});
+
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+
+  // Api Key
+  final _weatherService = WeatherServices(apiKey: dotenv.env['API_KEY'] ?? 'error');
+  Weather? _weather;
+
+  // Fetch Weather
+  _fetchWeather () async {
+    // Get Current City
+    String cityName = await _weatherService.getCurrentCity();
+
+    // Get Weather For City
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    // Error Handling
+    catch (e) {
+      print(e);
+    }
+  }
+
+  // Weather Animations
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // City Name
+            Text(
+              _weather?.cityName ?? "City.."
+            ),
+        
+            // Temperature
+            Text(
+              '${_weather?.temp.round()} °C' 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/weather_services.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherServices {
+
+  // ignore: constant_identifier_names
+  static const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall?';
+  final String apiKey;
+
+  WeatherServices({required this.apiKey});
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http.get(
+      Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric')
+    );
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(
+        jsonDecode(response.body)
+      );
+    } else {
+      throw Exception('Failed To Load Weather Data!');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    // Get Permission From User
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    // Get Current Location
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: LocationSettings(
+        accuracy: LocationAccuracy.high
+      )
+    );
+
+    // Convert Location To Placemark Object
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude, 
+      position.longitude,
+    );
+
+    // Exctract City Name
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
