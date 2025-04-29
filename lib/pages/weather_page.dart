7bb230cad77149e51da39e455aa8cfd494274331
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-04-30 00:46:44 +0530

    add basic info

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_services.dart';
@@ -12,13 +15,14 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-
   // Api Key
-  final _weatherService = WeatherServices(apiKey: dotenv.env['API_KEY'] ?? 'error');
+  final _weatherService = WeatherServices(
+    apiKey: dotenv.env['API_KEY'] ?? 'error',
+  );
   Weather? _weather;
-  
+
   // Fetch Weather
-  _fetchWeather () async {
+  _fetchWeather() async {
     // Get Current City
     String cityName = await _weatherService.getCurrentCity();
     print('City: $cityName');
@@ -30,7 +34,6 @@ class _WeatherAppState extends State<WeatherApp> {
         _weather = weather;
       });
     }
-
     // Error Handling
     catch (e) {
       print(e);
@@ -59,7 +62,7 @@ class _WeatherAppState extends State<WeatherApp> {
 
       case 'thunderstorm':
         return 'assets/thunder.json';
-      
+
       case 'clear':
         return 'assets/sunny.json';
 
@@ -77,33 +80,182 @@ class _WeatherAppState extends State<WeatherApp> {
 
   @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.sizeOf(context);
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // City Name
-            Text(
-              _weather?.cityName ?? "City.."
-            ),
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
 
-            // Animation
-            Lottie.asset(
-              getWeatherAnimation(_weather?.mainCondition)
-            ),
-        
-            // Temperature
-            Text(
-              '${_weather?.temp.round()} °C' 
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
 
-            // Weather Condition
-            Text(
-              _weather?.mainCondition ?? ""
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 1.2 * kToolbarHeight, 30, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              // Background
+              Align(
+                alignment: AlignmentDirectional(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+
+              Align(
+                alignment: AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF673AB7),
+                  ),
+                ),
+              ),
+
+              Align(
+                alignment: AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 900,
+                  decoration: BoxDecoration(color: Color(0xFFFFAB40)),
+                ),
+              ),
+
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 300.0, sigmaY: 100.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+              ),
+
+              // -------------------------------
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset('assets/location.png', height: 20),
+
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+
+                      // City Name
+                      Text(
+                        _weather?.cityName ?? "City..",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: MediaQuery.of(context).size.width * 0.02),
+
+                  Text(
+                    "Good Morning",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+
+                  SizedBox(height: media.height * 0.05),
+
+                  // Animated Image
+                  Center(
+                    child: Lottie.asset(
+                      getWeatherAnimation(_weather?.mainCondition),
+                    ),
+                  ),
+
+                  SizedBox(height: media.height * 0.05),
+
+                  // Temperature
+                  Center(
+                    child: Text(
+                      '${_weather?.temp.round()} °C',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 5),
+
+                  // Weather Condition
+                  Center(
+                    child: Text(
+                      _weather?.mainCondition.toUpperCase() ?? "",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 5),
+
+                  // Time And Day Info
+                  Center(
+                    child: Text(
+                      'Wednesday 20\t\t\t|\t\t\t12:00 AM',
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.white60
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: media.height * 0.05),
+
+                  
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+// Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             // City Name
+//             Text(
+//               _weather?.cityName ?? "City.."
+//             ),
+
+//             // Animation
+//             Lottie.asset(
+//               getWeatherAnimation(_weather?.mainCondition)
+//             ),
+        
+//             // Temperature
+//             Text(
+//               '${_weather?.temp.round()} °C' 
+//             ),
+
+//             // Weather Condition
+//             Text(
+//               _weather?.mainCondition ?? ""
+//             ),
+//           ],
+//         ),
+//       ),
